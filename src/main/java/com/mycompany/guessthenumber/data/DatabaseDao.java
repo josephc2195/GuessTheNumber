f03024f6fdb8b15befa025df6ba08c86ae2e761f
Joseph Chica <54424231+josephc2195@users.noreply.github.com>
2022-04-22 15:38:58 -0400

    Update DatabaseDao.java

--- src/main/java/com/mycompany/guessthenumber/data/DatabaseDao.java
+++ src/main/java/com/mycompany/guessthenumber/data/DatabaseDao.java
@@ -6,9 +6,12 @@ package com.mycompany.guessthenumber.data;
 
 import com.mycompany.guessthenumber.model.Game;
 import com.mycompany.guessthenumber.model.Round;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -26,7 +29,8 @@ public class DatabaseDao implements Dao{
 
     @Override
     public List<Game> getAllGames() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        final String cmd = "SELECT gameid, finnished, attempts FROM game";
+        return jdbcTemplate.query(cmd, new GameMapper());
     }
 
     @Override
@@ -44,4 +48,15 @@ public class DatabaseDao implements Dao{
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
+    private static final class GameMapper implements RowMapper<Game> {
+
+        @Override
+        public Game mapRow(ResultSet rs, int index) throws SQLException {
+            Game g1 = new Game();
+            g1.setGameId(rs.getInt("gameid"));
+            g1.setFinished(rs.getBoolean("finnished"));
+            g1.setAttempts(rs.getInt("attempts"));
+            return g1;
+        }
+    }
 }
