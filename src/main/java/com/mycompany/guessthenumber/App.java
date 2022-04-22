41e3759a7bd6f65c4683bf6440ee6ccf4fd7c075
Joseph Chica <54424231+josephc2195@users.noreply.github.com>
2022-04-22 14:49:49 -0400

    Update App.java

--- src/main/java/com/mycompany/guessthenumber/App.java
+++ src/main/java/com/mycompany/guessthenumber/App.java
@@ -9,9 +9,13 @@ package com.mycompany.guessthenumber;
  *
  * @author chica
  */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        SpringApplication.run(App.class, args);
     }
 }
